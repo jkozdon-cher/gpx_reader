49c6e0040a890d17d711341af42a8cd486843787
jkozdon-cher <jkozdon@o2.pl>
2026-01-02 13:46:59 +0100

    reader prepared to many files

--- gpx_reader.py
+++ gpx_reader.py
@@ -1,22 +1,28 @@
-import folium
 import gpxpy
-import matplotlib.pyplot as plt
 import pandas as pd
-import plotly.graph_objects as go
-import webbrowser
-from geopy.distance import geodesic
-from datetime import timedelta
-from pathlib import Path
+from base_track import BaseTrack
 
 
-class GpxTrack:
+class GpxTrack(BaseTrack):
+    """
+    Class for separate gpx track
+    """
     def __init__(self, gpx_file: str):
-        self.gpx_file = gpx_file
-        self.df = self._load_gpx()
+        df = self._load_gpx(gpx_file)
+        super().__init__(df)
 
-    def _load_gpx(self) -> pd.DataFrame:
+    def _load_gpx(self, gpx_file: str) -> pd.DataFrame:
+        """
+        Load gpx file and save data into data frame with columns:
+        lat - latitude
+        lon - longitude
+        ele - elevation
+        time - time per point
+        :param gpx_file: path to gpx file
+        :return: pd.DataFrame with data from gpx file
+        """
         rows = []
-        with open(self.gpx_file, 'r', encoding='utf-8') as f:
+        with open(gpx_file, 'r', encoding='utf-8') as f:
             gpx = gpxpy.parse(f)
 
         for track in gpx.tracks:
@@ -33,135 +39,22 @@ class GpxTrack:
         df = df.dropna(subset=['lat', 'lon'])
         return df
 
-    @property
-    def total_time(self):
-        start = self.df['time'].iloc[0]
-        end = self.df['time'].iloc[-1]
-        seconds = (end - start).total_seconds()
-        return timedelta(seconds=int(seconds))
 
-    @property
-    def distance(self) -> float:
-        distance = 0.0
-        for i in range(1, len(self.df)):
-            p1 = (self.df.iloc[i - 1]['lat'], self.df.iloc[i - 1]['lon'])
-            p2 = (self.df.iloc[i]['lat'], self.df.iloc[i]['lon'])
-            distance += geodesic(p1, p2).meters
-        return round(distance / 1000, 2)
-
-    @property
-    def moving_time(self):
-        min_speed_kmh = 0.5
-        min_distance_m = 1.0
-        stop_tolerance_s = 5.0
-
-        moving_seconds = 0.0
-        stopped_seconds = 0.0
-        for i in range(1, len(self.df)):
-            p1 = self.df.iloc[i - 1]
-            p2 = self.df.iloc[i]
-
-            if p1['time'] is None or p2['time'] is None:
-                continue
-
-            dt = (p2['time'] - p1['time']).total_seconds()
-            if dt <= 0:
-                continue
-
-            dist = geodesic((p1['lat'], p1['lon']),
-                            (p2['lat'], p2['lon'])
-                            ).meters
-            speed_kmh = (dist / dt) * 3.6 if dt > 0 else 0
-
-            if dist >= min_distance_m and speed_kmh >= min_speed_kmh:
-                moving_seconds += dt
-                stopped_seconds = 0
-            else:
-                stopped_seconds += dt
-                if stopped_seconds <= stop_tolerance_s:
-                    moving_seconds += dt
-
-        return timedelta(seconds=int(moving_seconds))
-
-    def generate_elevation_profile(self):
-        df = self.df.copy()
-        distances = [0.0]
-
-        for i in range(1, len(df)):
-            p1 = (df.iloc[i - 1]['lat'], df.iloc[i - 1]['lon'])
-            p2 = (df.iloc[i]['lat'], df.iloc[i]['lon'])
-            distances.append(distances[-1] + geodesic(p1, p2).meters / 1000)
-
-        df['dist_km'] = distances
-
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(
-            x=df['dist_km'],
-            y=df['ele'],
-            mode='lines',
-            fill='tozeroy',
-            name='Wysokość'
-        ))
-
-        fig.update_layout(
-            autosize=True,
-            height=250,
-            margin=dict(l=20, r=20, t=30, b=30),
-            showlegend=False
-        )
-        return fig.to_html(include_plotlyjs='cdn', full_html=False)
-
-    def stats_html(self):
-        return f"""
-        <ul>
-            <li><b>Dystans:</b> {self.distance / 1000:.2f} km</li>
-            <li><b>Czas całkowity:</b> {self.total_time}</li>
-            <li><b>Czas jazdy:</b> {self.moving_time}</li>
-        </ul>
-        """
-
-    def show_map(self, color='red', output_file='map.html', open_browser=True):
-        start = self.df.iloc[0]
-
-        m = folium.Map(location=[start['lat'], start['lon']],
-                       zoom_start=13,
-                       tiles='OpenStreetMap')
-
-        points = list(zip(self.df['lat'], self.df['lon']))
-        folium.PolyLine(points, color=color, weigh=4, opacity=0.8).add_to(m)
-
-        folium.Marker(points[0], tooltip='Start').add_to(m)
-        folium.Marker(points[-1], tooltip='Finish').add_to(m)
-
-        html = f"""
-            <div style="position: fixed;
-                        bottom: 0;
-                        left: 0;
-                        width: 100%;
-                        max-width: 100%,
-                        background: white;
-                        padding: 10px;
-                        z-index: 9999;
-                        border-radius: 8px;
-                        box-shadow: 0 0 10px rgba(0,0,0,0.3);">
-                {self.stats_html()}
-                {self.generate_elevation_profile()}
-            </div>
-            """
-
-        m.get_root().html.add_child(folium.Element(html))
-        m.save(output_file)
-
-        if open_browser:
-            path = Path(output_file).resolve()
-            webbrowser.open(path.as_uri())
-
-        return output_file
+class GxpRoute(BaseTrack):
+    """
+    Class for combined gpx tracks - ready route
+    """
+    def __init__(self, gpx_files):
+        dfs = [GpxTrack(f).df for f in gpx_files]
+        df = pd.concat(dfs, ignore_index=True)
+        df = df.sort_values('time').reset_index(drop=True)
+        super().__init__(df)
 
 
-file = 'test.gpx'
-track = GpxTrack(file)
-# print(f'czas: {track.total_time}')
-# print(f'czas jazdy: {track.moving_time}')
-# print(f'dystans: {track.distance} km')
-track.show_map(color='blue')
+route = GxpRoute([
+    "tracks/day_1.gpx",
+    "tracks/day_2_1.gpx",
+    "tracks/day_2_2.gpx",
+    "tracks/day_3.gpx"
+])
+route.show_map()
